1adde986dad6cea4e7e1cd9a2d523a8b6550ab81
Beckmann05 <lucasbeck@outlook.com>
2025-06-24 00:05:33 -0300

    footer em todas paginas

--- js/SAC.js
+++ js/SAC.js
@@ -249,23 +249,31 @@ function footer (){
   let footerContainer = document.getElementById('footer');
   footerContainer.innerHTML = `
   <div class = "container-footer">
-  <div class = "container">
-  <div class = "row">
-  <div class = "col-lg-3 col-md-12 com-sm-12 centro">
-  <div class = "logo logo-telefone"><i class="bi bi-telephone-inbound-fill"></i> (12) 32765-4321</div>
-  </div>
+    <div class = "container">
+      <div class = "row">
+        <div class = "col-lg-6 col-md-12 com-sm-12 d-flex my-3 centro">
+          <div class = "logo logo-instagram rounded text-center">
+            <a href="" class = "footer-icons">
+              <i class="bi bi-instagram"></i>@EcoClean
+            </a>
+          </div>
+          <div class = "logo text-center logo-whatsapp rounded">
+            <a href="" class = "footer-icons">
+              <i class="bi bi-whatsapp"></i>9812-5768
+            </a>
+          </div>
+          <div class = "logo text-center logo-facebook rounded">
+            <a href="" class = "footer-icons">
+              <i class="bi bi-facebook"></i>Eco_Clean
+            </a>
+          </div>
+        </div>
 
-  <div class = "col-lg-6 col-md-12 com-sm-12 d-flex my-3 centro">
-  <div class = "logo logo-instagram rounded"><a href="" class = "footer-icons"><i class="bi bi-instagram"></i>@EcoCleanOFC</a></div>
-  <div class = "logo logo-whatsapp rounded"><a href="" class = "footer-icons"><i class="bi bi-whatsapp"></i>(12)9812-5768</a></div>
-  <div class = "logo logo-facebook rounded"><a href="" class = "footer-icons"><i class="bi bi-facebook"></i>EcoCleanLTDA</a></div>
-  </div>
-
-  <div class = "col-lg-3 col-md-12 com-sm-12 marca-dagua centro">
-  <p>Todos os direitos reservados &copy;EcoClean LTDA<p>
-  </div>
-  </div>
-  </div>
+        <div class = "text-center col-lg-6 col-md-12 com-sm-12 marca-dagua centro justify-content-center">
+          <p>Todos os direitos reservados <br> &copy;EcoClean LTDA.<p>
+        </div>
+      </div>
+    </div>
   </div>
   `
 }
